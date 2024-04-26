4031c513a5cb00f932cc198cccc65f6bd7f1d48e
antrandothanh <trandothanhan04@gmail.com>
2024-04-27 03:23:00 +0700

    Repo init

---
+++ src/main/java/com/project/WebsiteKinhDoanhTheThao/WebsiteKinhDoanhTheThaoApplication.java
@@ -0,0 +1,13 @@
+package com.project.WebsiteKinhDoanhTheThao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteKinhDoanhTheThaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteKinhDoanhTheThaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/WebsiteKinhDoanhTheThao/WebsiteKinhDoanhTheThaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.WebsiteKinhDoanhTheThao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteKinhDoanhTheThaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
